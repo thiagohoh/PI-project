5a0661d87814736613508b8b510e28901b262c06
Thiago <thiago14lost@gmail.com>
2017-11-27 22:16:19 -0200

    database is now working fully

--- app/models/aluno.rb
+++ app/models/aluno.rb
@@ -2,6 +2,7 @@ class Aluno < ApplicationRecord
   belongs_to :user
   enum status: [:active, :inactive]
   has_many :actions
-  has_many :salas
+  #has_many :salas
+  has_and_belongs_to_many :salas
   mount_uploader :photo, PhotoUploader
 end
--- app/models/sala.rb
+++ app/models/sala.rb
@@ -1,5 +1,6 @@
 class Sala < ApplicationRecord
   enum status: [:active, :inactive]
   belongs_to :user
+  has_and_belongs_to_many :alunos
   mount_uploader :photo, PhotoUploader
 end
