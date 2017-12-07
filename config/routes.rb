98ac1a5fb2272850864005a50ae608924fb454bd
EricaSaito <eriyuri1213@gmail.com>
2017-12-07 21:31:16 -0200

    Add rasp to routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'home#index'
 
+  resources :rasp
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
